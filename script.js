81c959bddb232d0b947ee87371add9a2a8447241
clumsy261 <rucareanueric@gmail.com>
2026-08-07 19:44:33 +0300

    testing live page

---
+++ script.js
@@ -0,0 +1,2 @@
+document.querySelector("#welcome_image").innerHTML="<img src=\"https://apod.nasa.gov/apod/image/2608/noirlab2618b_1024.jpg\"></img>"
+dragElement(document.getElementById("#welcome_image"));
